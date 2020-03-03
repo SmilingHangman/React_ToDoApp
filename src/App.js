7dd429bbac7cf01971fbeace93e1c4d97f47fd38
SmilingHangman <andrejus.chomicenka@gmail.com>
2020-03-03 23:08:24 +0200

    basic functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './App.module.scss'
 
 function App() {
+
+const [currentTodo, setCurrentTodo] = useState("")
+const [todos, setTodos] = useState([])
+
   return (
     <div className={classes.container}>
+      <input type="text" placeholder="Write a task" onInput={event => setCurrentTodo(event.target.value)}/>
+      <button onClick={() => setTodos([...todos, { currentTodo, id: String(Math.random()) }])}>Add task</button>
+      <ul className={classes.tasklist}>
+            {todos.map(todo => (
+              <li key={todo.id}> {todo.currentTodo} <button onClick={() => setTodos(todos.filter(el => todo.id !== el.id))}>Delete</button>
+              </li>
+              ))}
+      </ul>
     </div>
   );
 }
